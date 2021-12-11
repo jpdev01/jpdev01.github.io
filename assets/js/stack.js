e15e3ac3643cec7faca651ebb22a501941e76758
Joao Pedro Truchinski Borba <joao.borba@asaas.com.br>
2021-12-11 13:05:04 -0300

    await

--- assets/js/stack.js
+++ assets/js/stack.js
@@ -1,5 +1,5 @@
 function render(){
-     jQuery.get("assets/mock/stack.json", undefined, function(data) {
+    jQuery.get("assets/mock/stack.json", undefined, function(data) {
         let stack = data.stack;
         return stack;
     });
