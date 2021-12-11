b50fd0979a6f32c9fd9f20f81522d4e71b96d7bd
Joao Pedro Truchinski Borba <joao.borba@asaas.com.br>
2021-12-11 12:59:07 -0300

    read file test

--- assets/js/stack.js
+++ assets/js/stack.js
@@ -1,5 +1,5 @@
 function render(){
-    await jQuery.get("assets/mock/stack.json", undefined, function(data) {
+     jQuery.get("assets/mock/stack.json", undefined, function(data) {
         let stack = data.stack;
         return stack;
     });
