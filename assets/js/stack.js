769e5cd4b69cb6b42fdbbeb5b4e899d968e90159
Joao Pedro Truchinski Borba <joao.borba@asaas.com.br>
2021-12-16 13:41:25 -0300

    indent

--- assets/js/stack.js
+++ assets/js/stack.js
@@ -1,16 +1,6 @@
-// async function render(){
-//     debugger;
-//     await jQuery.get("assets/mock/stack.json", undefined, function(data) {
-//         let stack = data.stack;
-//         return stack;
-//     });
-// }
-
 $(document).ready(function() {
     jQuery.get("../../assets/mock/stack.json", undefined, function(data) {
-let stack = data.stack;
-console.log(stack);
-$(".dock").html(getStackHtml(stack));
+$(".dock").html(getStackHtml(data.stack));
 });
 });
 
